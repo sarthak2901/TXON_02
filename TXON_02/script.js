8990af5f6e58ddf553361b40be8aeef767cbbb5f
sarthak2901 <113253969+sarthak2901@users.noreply.github.com>
2023-02-19 20:39:30 +0530

    Add files via upload

---
+++ TXON_02/script.js
@@ -0,0 +1,13 @@
+function addToScreen(value) {
+  document.getElementById("screen").value += value;
+}
+
+function clearScreen() {
+  document.getElementById("screen").value = "";
+}
+
+function calculate() {
+  let expression = document.getElementById("screen").value;
+  let result = eval(expression);
+  document.getElementById("screen").value = result;
+}
